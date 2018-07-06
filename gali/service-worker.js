acc95e1ed5930992598fd71df7a78c85de0d8aa8
Francesc Busquets <francesc@gmail.com>
2018-07-06 13:27:02 +0200

    Optimitzacions en disseny

--- gali/service-worker.js
+++ gali/service-worker.js
@@ -48,13 +48,10 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+    caches.keys()
+      .then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName)))
+      .then(cachesToDelete => Promise.all(cachesToDelete.map(ch => caches.delete(ch))))
+      .then(() => self.clients.claim())
   );
 });
 
@@ -65,19 +62,20 @@ self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse)
-          return cachedResponse;
+      caches.match(event.request)
+        .then(cachedResponse => {
+          if (cachedResponse)
+            return cachedResponse;
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
+          return caches.open(RUNTIME)
+            .then(cache => {
+              return fetch(event.request)
+                .then(response => {
+                  return cache.put(event.request, response.clone())
+                    .then(() => response);
+                });
             });
-          });
-        });
-      })
+        })
     );
   }
 });
