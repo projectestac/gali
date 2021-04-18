ec8bd0341fb31179564d6837186f94ad902d412f
Francesc Busquets <francesc@gmail.com>
2021-04-18 20:47:38 +0200

    SW with workbox (WIP)

--- gali/service-worker.js
+++ gali/service-worker.js
@@ -1,81 +1,120 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+/* eslint-disable no-restricted-globals */
+/* global importScripts */
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v3';
-const RUNTIME = 'runtime';
+if ('function' === typeof importScripts) {
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'gali.css',
-  'gali.html',
-  'gali.js',
-  'player.html',
-  'img/logo_petit.gif',
-  'img/led_taronja.gif',
-  'img/led_taronja_ok.gif',
-  'img/led_verd.gif',
-  'img/led_verd_ok.gif',
-  'img/selecciona.gif',
-  'img/selecciona-oc.gif',
-  'js/jquery-3.2.1.min.js'
-];
+  // Catch possible "SKIP_WAITING" events
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName)))
-      .then(cachesToDelete => Promise.all(cachesToDelete.map(ch => caches.delete(ch))))
-      .then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(cachedResponse => {
-          if (cachedResponse)
-            return cachedResponse;
-
-          return caches.open(RUNTIME)
-            .then(cache => {
-              return fetch(event.request)
-                .then(response => {
-                  return cache.put(event.request, response.clone())
-                    .then(() => response);
-                });
-            });
+  // Clean old caches created with `sw-precache`
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys()
+        .then(cacheNames => {
+          return Promise.all(
+            cacheNames
+              .filter(cacheName => /^(precache-v3|runtime)+/.test(cacheName))
+              .map(cacheName => {
+                console.log(`SW - Deleting old cache "${cacheName}"`);
+                return caches.delete(cacheName);
+              })
+          );
         })
     );
-  }
-});
+  });
+
+  /* global workbox */
+  if (workbox) {
+
+    // Set debug mode based on search params when registering, like: `sw.js?debug`
+    // From: https://stackoverflow.com/questions/50795315/workbox-set-debug-mode-dynamically
+    const url = new URL(location.href);
+    const debug = url.searchParams.has('debug');
+    workbox.setConfig({ debug });
+
+    // Import statements
+    const {
+      core: { setCacheNameDetails, clientsClaim },
+      precaching: { precacheAndRoute, createHandlerBoundToURL },
+      routing: { NavigationRoute, registerRoute },
+      strategies: { StaleWhileRevalidate, CacheFirst },
+      expiration: { ExpirationPlugin },
+      cacheableResponse: { CacheableResponsePlugin },
+      recipes: { googleFontsCache },
+    } = workbox;
+
+    // Set a specific prefix for this SW, used in cache names
+    setCacheNameDetails({
+      prefix: 'gali',
+    });
+
+    // Take control immediatly
+    clientsClaim();
+
+    // Injection point for manifest files
+    precacheAndRoute([
+      { 'url': 'index.html', 'revision': '20210418' },
+      { 'url': 'gali.css', 'revision': '20210418' },
+      { 'url': 'gali.html', 'revision': '20210418' },
+      { 'url': 'gali.js', 'revision': '20210418' },
+      { 'url': 'player.html', 'revision': '20210418' },
+      { 'url': 'img/logo_petit.gif', 'revision': '20210418' },
+      { 'url': 'img/led_taronja.gif', 'revision': '20210418' },
+      { 'url': 'img/led_taronja_ok.gif', 'revision': '20210418' },
+      { 'url': 'img/led_verd.gif', 'revision': '20210418' },
+      { 'url': 'img/led_verd_ok.gif', 'revision': '20210418' },
+      { 'url': 'img/selecciona.gif', 'revision': '20210418' },
+      { 'url': 'img/selecciona-oc.gif', 'revision': '20210418' },
+      { 'url': 'js/jquery-3.6.0.min.js', 'revision': '20210418' },
+    ]);
+
+    // custom cache rules
+    const handler = createHandlerBoundToURL('./index.html');
+    const navigationRoute = new NavigationRoute(handler, {
+      denylist: [/^\/_/, /\/[^/]+\.[^/]+$/],
+    });
+    registerRoute(navigationRoute);
+
+    // Cache for activities
+    registerRoute(
+      /^https:\/\/clic\.xtec\.cat\/projects\//,
+      new StaleWhileRevalidate({
+        cacheName: 'gali-activities',
+        plugins: [
+          new CacheableResponsePlugin({
+            statuses: [200],
+          }),
+          new ExpirationPlugin({
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 7, // 7 Days
+            purgeOnQuotaError: true,
+          }),
+        ],
+      }),
+    );
+
+    // Use the new recipe for Google Fonts in WorkBox 6, instead of the full pattern
+    googleFontsCache();
+
+    // Cache for big logos and miscellaneous icons (small logos are always pre-cached)
+    registerRoute(
+      /\/img\/[/\w]*\.(?:jpg|gif|png)$/,
+      new CacheFirst({
+        cacheName: 'gali-img-cache',
+        plugins: [
+          new ExpirationPlugin({
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days            
+          }),
+        ],
+      })
+    );
+
+  } else
+    console.log('Workbox could not be loaded. No offline support!');
+}
